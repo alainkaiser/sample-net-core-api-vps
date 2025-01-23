0622f2ef1d1cc7d7e9ad04d678d2467203db1cf1
Alain Kaiser <kaiseralain1@gmail.com>
2025-01-23 10:43:34 +0100

    fix: add gitignore

--- SampleWebApi/obj/Debug/net9.0/SampleWebApi.AssemblyInfo.cs
+++ SampleWebApi/obj/Debug/net9.0/SampleWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753636d48a20d0a0328f309d0694a777e6eb61f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
