fb551ba76e8ee9917ddc3856d5958df21b9e6d19
Alain Kaiser <kaiseralain1@gmail.com>
2025-01-27 16:04:43 +0100

    wip

--- SampleWebApi/Program.cs
+++ SampleWebApi/Program.cs
@@ -2,19 +2,24 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Kestrel to use HTTPS
+// Kestrel HTTPS-Konfiguration
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(443,
-        listenOptions => { listenOptions.UseHttps("/etc/ssl/certs/webapi.crt", "/etc/ssl/certs/webapi.key"); });
+    options.ListenAnyIP(80); // HTTP
+    options.ListenAnyIP(443, listenOptions =>
+    {
+        listenOptions.UseHttps("/https/selfsigned.pem");
+    });
 });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }); });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
